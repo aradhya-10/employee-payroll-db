475618946b9bda960f1d8505896f15cf7c7a187d
aradhya-10 <202051034@iiitvadodara.ac.in>
2023-12-18 19:23:59 +0530

    Read from the database

--- src/EmployeeQueries.sql
+++ src/EmployeeQueries.sql
@@ -41,3 +41,16 @@ mysql> INSERT INTO employee_payroll (name, salary, start_date) VALUES
 Query OK, 4 rows affected (0.00 sec)
 Records: 4  Duplicates: 0  Warnings: 0
 
+
+-- UC4: Reading all data from the table
+mysql> SELECT * FROM employee_payroll;
++--------+---------+--------+------------+
+| emp_id | name    | salary | start_date |
++--------+---------+--------+------------+
+|      1 | Bill    |  10000 | 2022-10-11 |
+|      2 | Alice   |  20000 | 2023-05-09 |
+|      3 | Charlie |  35000 | 2023-01-01 |
+|      4 | Dave    |  25000 | 2023-11-01 |
++--------+---------+--------+------------+
+4 rows in set (0.00 sec)
+
